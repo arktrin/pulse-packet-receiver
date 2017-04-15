c03a6baf44c16f074340c95fb8a36ddf4f2c7029
en <encliss@gmail.com>
2017-04-15 13:51:13 +0300

    at(1) works with schedule app finally

--- schedule_app.py
+++ schedule_app.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 
-from PyQt4 import QtGui  # Import the PyQt4 module we'll need
-import sys  # We need sys so that we can pass argv to QApplication
+from PyQt4 import QtGui
+import sys, os, subprocess
+import template_schedule
 
-import template_schedule  # This file holds our MainWindow and all design related things
-
-# it also keeps events etc that we defined in Qt Designer
-import os  # For listing directory methods
-
-
-class ExampleApp(QtGui.QMainWindow, template_schedule.Ui_MainWindow):
+class ScheduleApp(QtGui.QMainWindow, template_schedule.Ui_MainWindow):
 	def __init__(self):
 		super(self.__class__, self).__init__()
 		self.setupUi(self)  
 		self.tableWidget.setColumnWidth(2, 160)
+		self.addTaskBtn.clicked.connect(self.add_task)
+
+	def add_task(self):
+		start = str(self.startTimeEdit.text()).strip()
+		end = str(self.endTimeEdit.text()).strip()
+		threshold = str(self.thresholdValSpin.value())
+		text_for_at = ' '.join(start[:-3].split(' ')[::-1])
+		bash_command = 'echo "env DISPLAY=:0 pulse_packet_receiver.py" '+start+' '+end+' '+threshold+' | at '+text_for_at
+		process = subprocess.Popen(['bash','-c', bash_command], stdout=subprocess.PIPE)
+		output, error = process.communicate()
+		print output, error
 
 
 def main():
 	app = QtGui.QApplication(sys.argv)  # A new instance of QApplication
-	form = ExampleApp()  # We set the form to be our ExampleApp (design)
+	form = ScheduleApp()
 	form.show()  # Show the form
 	app.exec_()  # and execute the app
 
